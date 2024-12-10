08019e935351cddb0e28dc7924d77daed4047cf5
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2024-12-10 12:00:49 +0100

    set up coffee

---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <div className="w-full p-4 flex justify-between bg-red-300 ">
+      <div>Lofo</div>
+      <div>
+        <div>
+          textinput
+          <div>conextion panier</div>
+        </div>
+        <div>blabal</div>
+      </div>
+    </div>
+  );
+}
