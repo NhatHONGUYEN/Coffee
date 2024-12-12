37b0a90a9599f27ff5024bc18b99b1264865a53f
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2024-12-12 10:57:13 +0100

    ✨ Hook : create useAuth for authentification

---
+++ src/app/hook/useAuth.jsx
@@ -0,0 +1,95 @@
+import { useRouter } from "next/navigation";
+import { auth, db } from "../api/firebaseConfig";
+import { setDoc } from "firebase/firestore";
+import { useEffect } from "react";
+
+const {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} = require("firebase/auth");
+
+const providerGoogle = new GoogleAuthProvider();
+
+export default function useAuth() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, providerGoogle);
+      setUser(result);
+      router.push("/");
+    } catch (error) {
+      console.error("Erreur lors de la connexion avec Google:", error);
+      setError(
+        "Une erreur s'est produite lors de la connexion avec Google. Veuillez réessayer."
+      );
+    }
+  };
+
+  const signUpWithEmailAndPassword = async (email, password, username) => {
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = result.user;
+
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        email: user.email,
+        username: username,
+      });
+      setUser(user);
+      router.push("/");
+    } catch (error) {
+      console.error("Erreur lors de la création du compte:", error);
+      setError(
+        "Une erreur s'est produite lors de la création du compte. Veuillez réessayer."
+      );
+    }
+  };
+
+  const signInWithEmailAndPassword = async (email, password) => {
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      setUser(result.user);
+      router.push("/");
+    } catch (error) {
+      console.error("Erreur lors de la connexion:", error);
+      setError(
+        "Une erreur s'est produite lors de la connexion. Veuillez réessayer."
+      );
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const moveToHome = () => {
+    if (user) {
+      router.push("/");
+    }
+  };
+
+  return {
+    user,
+    error,
+    loginWithGoogle,
+    signUpWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    moveToHome,
+  };
+}
