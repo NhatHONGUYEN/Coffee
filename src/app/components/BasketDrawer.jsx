c3c43cfd65359bdc3b3c88dcb23e43b048b2f583
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2024-12-13 16:44:24 +0100

    💄 Basket : add decremente and increment input

--- src/app/components/BasketDrawer.jsx
+++ src/app/components/BasketDrawer.jsx
@@ -8,9 +8,10 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 
 import { useBasket } from "../context/BasketContext";
 import BasketDrawerArticles from "./BasketDrawerArticles";
+import { Button } from "@/components/ui/button";
 
 export default function BasketDrawer({ isOpen, onClose }) {
-  const { basket, removeItem, clearBasket, totalPrice } = useBasket();
+  const { basket, removeItem, totalPrice } = useBasket();
 
   return (
     <Dialog open={isOpen} onClose={onClose} className="relative z-10">
@@ -26,62 +27,41 @@ export default function BasketDrawer({ isOpen, onClose }) {
               transition
               className="pointer-events-auto w-screen max-w-md transform transition duration-500 ease-in-out data-[closed]:translate-x-full sm:duration-700"
             >
-              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+              <div className="flex h-full justify-between flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                 <div className="px-4 sm:px-6">
                   <div className="flex items-start justify-between">
-                    <DialogTitle className="text-base font-semibold text-gray-900">
+                    <DialogTitle className="text-base font-semibold py-10 text-gray-900">
                       Votre Panier
                     </DialogTitle>
-                    <div className="ml-3 flex h-7 items-center">
-                      <button
-                        type="button"
-                        onClick={onClose}
-                        className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        <span className="absolute -inset-2.5" />
-                        <span className="sr-only">Close panel</span>
-                        <XMarkIcon aria-hidden="true" className="size-6" />
-                      </button>
-                    </div>
+                  </div>
+                  <div className="mt-2 ">
+                    {basket.length > 0 ? (
+                      <div>
+                        <ul>
+                          {basket.map((item) => (
+                            <BasketDrawerArticles
+                              key={item.id}
+                              item={item}
+                              onRemove={removeItem}
+                            />
+                          ))}
+                        </ul>
+                        <div className="font-bold flex justify-between mx-auto w-80 border-t-2 border-gray-300 pt-10">
+                          Total: <div>{totalPrice} €</div>
+                        </div>
+                      </div>
+                    ) : (
+                      <p className="text-sm text-gray-500">
+                        Votre panier est vide.
+                      </p>
+                    )}
                   </div>
                 </div>
-                <div className="mt-2">
-                  {basket.length > 0 ? (
-                    <div>
-                      <ul>
-                        {basket.map((item) => (
-                          <BasketDrawerArticles
-                            key={item.id}
-                            item={item}
-                            onRemove={removeItem}
-                          />
-                        ))}
-                      </ul>
-                      <div>{totalPrice} </div>
-                    </div>
-                  ) : (
-                    <p className="text-sm text-gray-500">
-                      Votre panier est vide.
-                    </p>
-                  )}
-                </div>
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={onClose}
-                  >
-                    Fermer
-                  </button>
-                  {basket.length > 0 && (
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2 ml-2"
-                      onClick={clearBasket}
-                    >
-                      Vider le panier
-                    </button>
-                  )}
+
+                <div className="p-6 ">
+                  <Button className="w-full" onClick={onClose}>
+                    Continue to shopping
+                  </Button>
                 </div>
               </div>
             </DialogPanel>
--- src/app/components/BasketDrawerArticles.jsx
+++ src/app/components/BasketDrawerArticles.jsx
@@ -1,11 +1,11 @@
 // components/BasketDrawerArticles.jsx
 import { Button } from "@/components/ui/button";
-import { InputOTPSeparator } from "@/components/ui/input-otp";
+
 import React from "react";
 import { useBasket } from "../context/BasketContext";
 
 const BasketDrawerArticles = ({ item }) => {
-  const { addItem, removeItem, clearItem } = useBasket();
+  const { addItem, removeItem } = useBasket();
 
   const removeItemQuantity = () => {
     removeItem(item.id);
@@ -15,25 +15,38 @@ const BasketDrawerArticles = ({ item }) => {
     addItem(item);
   };
 
-  const removeArticle = () => {
-    clearItem(item.id);
-  };
-
   const articleTotalPrice = (item.price * item.quantity).toFixed(2);
 
   return (
     <>
-      <li className="flex justify-between items-center mb-2">
-        <span>{item.name}</span>
-        <Button onClick={removeItemQuantity}>-</Button>
-        <InputOTPSeparator />
-        {item.quantity}
-        <InputOTPSeparator />
-        <Button onClick={addItemQuantity}>+</Button>
-        <span>{articleTotalPrice} €</span>
-        <button onClick={removeArticle} className="text-red-500">
-          Supprimer
-        </button>
+      <li className="flex border-t-2 border-gray-300  mx-6 h-40 justify-center items-center   pr-8  mb-2">
+        <div className="flex justify-center items-center h-full ">
+          <img
+            src={item.image_url}
+            alt={item.name}
+            className="w-32 h-32 object-cover"
+          />
+        </div>
+        <div className="flex ">
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">{item.name}</p>
+
+            <div className="flex w-56 justify-between items-center gap-4">
+              <p>{articleTotalPrice} €</p>
+              <div className="flex gap-4  items-center">
+                <Button variant="outline" onClick={removeItemQuantity}>
+                  -
+                </Button>
+
+                {item.quantity}
+
+                <Button variant="outline" onClick={addItemQuantity}>
+                  +
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </li>
     </>
   );
