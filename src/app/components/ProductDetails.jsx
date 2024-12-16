bb0fe31d036c6ec9705e36807f38a346f698c67e
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2024-12-16 07:09:35 +0100

    ✨ Add Suggestions

--- src/app/components/ProductDetails.jsx
+++ src/app/components/ProductDetails.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { useBasket } from "../context/BasketContext";
 import { Button } from "@/components/ui/button";
+import Suggestions from "./Suggestions";
 
 export default function ProductDetails({ product }) {
   const { addItem } = useBasket();
@@ -12,39 +13,44 @@ export default function ProductDetails({ product }) {
   };
 
   return (
-    <div className="max-w-7xl pt-20 flex justify-center items-center">
-      <div className="lg:w-[4000px]">
-        <img src={product.image_url} alt={product.name} />
+    <>
+      <div className="max-w-7xl pt-20 flex justify-center items-center">
+        <div className="lg:w-[4000px]">
+          <img src={product.image_url} alt={product.name} />
+        </div>
+        <div className="flex flex-col gap-4 text-md">
+          <p>
+            <span className="font-bold">Name:</span> {product.name}
+          </p>
+          <p className="flex w-96 flex-col">
+            <span className="font-bold">Description:</span>{" "}
+            {product.description}
+          </p>
+          <p>
+            <span className="font-bold">Price:</span> {product.price} €
+          </p>
+          <p>
+            <span className="font-bold">Region:</span> {product.region}
+          </p>
+          <p>
+            <span className="font-bold">Weight:</span> {product.weight}
+          </p>
+          <p>
+            <span className="font-bold">Roast Level:</span>{" "}
+            {product.roast_level}
+          </p>
+          <p className="flex flex-col">
+            <span className="font-bold">Flavor Profile:</span>{" "}
+            {product.flavor_profile}
+          </p>
+          <p className="flex flex-col">
+            <span className="font-bold">Grind Option:</span>{" "}
+            {product.grind_option}
+          </p>
+          <Button onClick={handleAddItem}>Ajouter</Button>
+        </div>
       </div>
-      <div className="flex flex-col gap-4 text-md">
-        <p>
-          <span className="font-bold">Name:</span> {product.name}
-        </p>
-        <p className="flex w-96 flex-col">
-          <span className="font-bold">Description:</span> {product.description}
-        </p>
-        <p>
-          <span className="font-bold">Price:</span> {product.price} €
-        </p>
-        <p>
-          <span className="font-bold">Region:</span> {product.region}
-        </p>
-        <p>
-          <span className="font-bold">Weight:</span> {product.weight}
-        </p>
-        <p>
-          <span className="font-bold">Roast Level:</span> {product.roast_level}
-        </p>
-        <p className="flex flex-col">
-          <span className="font-bold">Flavor Profile:</span>{" "}
-          {product.flavor_profile}
-        </p>
-        <p className="flex flex-col">
-          <span className="font-bold">Grind Option:</span>{" "}
-          {product.grind_option}
-        </p>
-        <Button onClick={handleAddItem}>Ajouter</Button>
-      </div>
-    </div>
+      <Suggestions />
+    </>
   );
 }
---
+++ src/app/components/Suggestions.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useFetchProducts } from "../hook/useFetchProducts";
+import ProductCard from "./ProductCard";
+
+export default function Suggestions() {
+  const { products, error, loading } = useFetchProducts();
+
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
+
+  if (error) {
+    return <div>Erreur : {error}</div>;
+  }
+
+  const randomProducts = products
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4)
+    .map((product) => <ProductCard product={product} key={product.id} />);
+
+  return (
+    <div className="max-w-7xl mx-auto  ">
+      <h1 className="text-xl font-bold py-20  ">Suggestions</h1>
+      <div className="flex gap-10">{randomProducts}</div>;
+    </div>
+  );
+}
